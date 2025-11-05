25d08c7b023d452eef0672ec464509b8dc2ebee9
Melvin Whitaker <awsome7mailman@gmail.com>
2025-11-04 18:32:22 -0700

    metrics properly being created and displayed

--- src/metrics.js
+++ src/metrics.js
@@ -13,12 +13,12 @@ function getMemoryUsagePercentage() {
   const totalMemory = os.totalmem();
   const freeMemory = os.freemem();
   const usedMemory = totalMemory - freeMemory;
-  const memoryUsage = (usedMemory / totalMemory) * 100;
-  return memoryUsage.toFixed(2);
+  const memoryUsage = (usedMemory / totalMemory).toFixed(2) * 100;
+  return memoryUsage;
 }
 
 // Middleware to track requests
-export function requestTracker(req, res, next) {
+function requestTracker(req, res, next) {
   const endpoint = `[${req.method}] ${req.path}`;
   requests[endpoint] = (requests[endpoint] || 0) + 1;
   next();
@@ -29,57 +29,86 @@ export function requestTracker(req, res, next) {
 // }
 
 setInterval(() => {
+  const metrics = [];
+
+  Object.keys(requests).forEach((endpoint) => {
+    metrics.push(
+      createMetric("requests", requests[endpoint], "1", "sum", "asInt", {
+        endpoint,
+      })
+    );
+  });
+
   const cpuValue = getCpuUsagePercentage();
-  sendMetricToGrafana("cpu", cpuValue, "gauge", "%");
+  //   console.log(cpuValue);
+  metrics.push(createMetric("cpu", cpuValue, "%", "gauge", "asDouble"));
 
   const memoryValue = getMemoryUsagePercentage();
-  sendMetricToGrafana("memory", memoryValue, "gauge", "%");
-
-  sendMetricToGrafana("requests", requests, "sum", "1");
+  //   console.log(memoryValue);
+  metrics.push(createMetric("memory", memoryValue, "%", "gauge", "asDouble"));
 
   //   latency += Math.floor(Math.random() * 200) + 1;
   //   sendMetricToGrafana("latency", latency, "sum", "ms");
+
+  sendMetricToGrafana(metrics);
 }, 1000);
 
-function sendMetricToGrafana(metricName, metricValue, type, unit) {
+function createMetric(
+  metricName,
+  metricValue,
+  metricUnit,
+  metricType,
+  valueType,
+  attributes
+) {
+  attributes = { ...attributes, source: config.source };
+
+  const metric = {
+    name: metricName,
+    unit: metricUnit,
+    [metricType]: {
+      dataPoints: [
+        {
+          [valueType]: metricValue,
+          timeUnixNano: Date.now() * 1000000,
+          attributes: [],
+        },
+      ],
+    },
+  };
+
+  Object.keys(attributes).forEach((key) => {
+    metric[metricType].dataPoints[0].attributes.push({
+      key: key,
+      value: { stringValue: attributes[key] },
+    });
+  });
+
+  if (metricType === "sum") {
+    metric[metricType].aggregationTemporality =
+      "AGGREGATION_TEMPORALITY_CUMULATIVE";
+    metric[metricType].isMonotonic = true;
+  }
+
+  return metric;
+}
+
+function sendMetricToGrafana(metrics) {
   const metric = {
     resourceMetrics: [
       {
         scopeMetrics: [
           {
-            metrics: [
-              {
-                name: metricName,
-                unit: unit,
-                [type]: {
-                  dataPoints: [
-                    {
-                      asInt: metricValue,
-                      timeUnixNano: Date.now() * 1000000,
-                    },
-                  ],
-                },
-              },
-            ],
+            metrics,
           },
         ],
       },
     ],
   };
 
-  if (type === "sum") {
-    metric.resourceMetrics[0].scopeMetrics[0].metrics[0][
-      type
-    ].aggregationTemporality = "AGGREGATION_TEMPORALITY_CUMULATIVE";
-    metric.resourceMetrics[0].scopeMetrics[0].metrics[0][
-      type
-    ].isMonotonic = true;
-  }
-
-  const body = JSON.stringify(metric);
   fetch(`${config.metrics.url}`, {
     method: "POST",
-    body: body,
+    body: JSON.stringify(metric),
     headers: {
       Authorization: `Bearer ${config.metrics.apiKey}`,
       "Content-Type": "application/json",
@@ -89,14 +118,16 @@ function sendMetricToGrafana(metricName, metricValue, type, unit) {
       if (!response.ok) {
         response.text().then((text) => {
           console.error(
-            `Failed to push metrics data to Grafana: ${text}\n${body}`
+            `Failed to push metrics data to Grafana: ${text}\n${metric}`
           );
         });
       } else {
-        console.log(`Pushed ${metricName}`);
+        console.log(`Pushed metrics to Grafana`);
       }
     })
     .catch((error) => {
       console.error("Error pushing metrics:", error);
     });
 }
+
+module.exports = { requestTracker };
